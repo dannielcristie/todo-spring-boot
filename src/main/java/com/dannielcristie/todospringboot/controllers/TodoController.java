d591ed435b1569fd273ddcf4f8b02b8f708d63a8
Danniel Cristie <dannielcristie@gmail.com>
2023-07-31 10:06:55 -0300

    delete todo by id controller

--- src/main/java/com/dannielcristie/todospringboot/controllers/TodoController.java
+++ src/main/java/com/dannielcristie/todospringboot/controllers/TodoController.java
@@ -3,6 +3,7 @@ package com.dannielcristie.todospringboot.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,12 @@ public class TodoController {
     ResponseEntity<Todo> update(@PathVariable Long id, @RequestBody Todo obj) {
         obj = todoService.update(id, obj);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Void> delete(@PathVariable Long id) {
+        todoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
