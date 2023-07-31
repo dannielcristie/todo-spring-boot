b185360321b3c60a4e4d3afe8e327ccfc6fdec0b
Danniel Cristie <dannielcristie@gmail.com>
2023-07-31 08:41:08 -0300

    Todo Entity

---
+++ src/main/java/com/dannielcristie/todospringboot/entities/Todo.java
@@ -0,0 +1,70 @@
+package com.dannielcristie.todospringboot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_todos")
+public class Todo  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean done;
+    private Integer priority;
+
+    public Todo() {
+    }
+
+    public Todo(String name, String description, Boolean done, Integer priority) {
+        this.name = name;
+        this.description = description;
+        this.done = done;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+}
