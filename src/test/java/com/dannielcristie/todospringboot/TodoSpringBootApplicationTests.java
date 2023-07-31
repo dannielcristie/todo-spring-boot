f3ecf56caab3c5644f990a4ec78151e60042147e
Danniel Cristie <dannielcristie@gmail.com>
2023-07-31 08:17:54 -0300

    feature: create spring-boot project

---
+++ src/test/java/com/dannielcristie/todospringboot/TodoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannielcristie.todospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
