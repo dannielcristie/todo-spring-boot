07f2e843e1551e8faeb1f67e340e237476f85983
Danniel Cristie <dannielcristie@gmail.com>
2023-07-31 09:30:58 -0300

    find by id todo service

--- src/main/java/com/dannielcristie/todospringboot/services/TodoService.java
+++ src/main/java/com/dannielcristie/todospringboot/services/TodoService.java
@@ -3,6 +3,8 @@ package com.dannielcristie.todospringboot.services;
 import com.dannielcristie.todospringboot.entities.Todo;
 import com.dannielcristie.todospringboot.repositories.TodoRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +25,22 @@ public class TodoService {
         Sort sort = Sort.by("priority").descending().and(
                 Sort.by("name").ascending());
         return todoRepository.findAll(sort);
+    }
+
+    public Todo findById(Long id) {
+        Optional<Todo> obj = todoRepository.findById(id);
+        return obj.get();
+    }
+
+    public Todo update(Long id, Todo obj) {
+        Todo entity = todoRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return todoRepository.save(entity);
+    }
+
+    private void updateData(Todo entity, Todo obj) {
+        entity.setName(obj.getName());
+        entity.setDescription(obj.getDescription());
+        entity.setDone(obj.getDone());
     }
 }
