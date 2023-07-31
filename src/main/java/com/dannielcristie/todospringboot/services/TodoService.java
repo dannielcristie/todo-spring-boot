73395298ab4e828cf0232b6f730419894188a650
Danniel Cristie <dannielcristie@gmail.com>
2023-07-31 09:17:57 -0300

    create todo service

---
+++ src/main/java/com/dannielcristie/todospringboot/services/TodoService.java
@@ -0,0 +1,22 @@
+package com.dannielcristie.todospringboot.services;
+
+import com.dannielcristie.todospringboot.entities.Todo;
+import com.dannielcristie.todospringboot.repositories.TodoRepository;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public Todo create(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+}
