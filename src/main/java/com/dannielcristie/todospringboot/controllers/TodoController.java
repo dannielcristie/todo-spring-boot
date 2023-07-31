b62fa679e654106f44825a21a5712c0b528e00f2
Danniel Cristie <dannielcristie@gmail.com>
2023-07-31 09:58:27 -0300

    find all todo controller

--- src/main/java/com/dannielcristie/todospringboot/controllers/TodoController.java
+++ src/main/java/com/dannielcristie/todospringboot/controllers/TodoController.java
@@ -1,6 +1,9 @@
 package com.dannielcristie.todospringboot.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,10 @@ public class TodoController {
         return ResponseEntity.ok().body(obj);
     }
 
-    
+    @GetMapping
+    ResponseEntity<List<Todo>> findAll() {
+        List<Todo> list = todoService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 
 }
